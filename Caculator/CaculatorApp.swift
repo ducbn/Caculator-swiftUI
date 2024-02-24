a059ea7ebe66ce5b3e51617f688fbfa37774647f
Bùi Ngọc Đức <ducbui27924@gmail.com>
2024-02-25 00:45:35 +0700

    Initial Commit

---
+++ Caculator/CaculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaculatorApp.swift
+//  Caculator
+//
+//  Created by Bui Ngoc Duc on 25/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CaculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
